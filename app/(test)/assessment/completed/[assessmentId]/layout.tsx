aebaadca1ec2fda61e6d3fa9a406bee7a7c86c75
techDr2022 <info@techdr.in>
2025-04-22 12:10:10 +0530

    fix test assessment issues

--- app/(test)/assessment/completed/[assessmentId]/layout.tsx
+++ app/(test)/assessment/completed/[assessmentId]/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Toaster } from "react-hot-toast";
+import { AssessmentProvider } from "@/lib/store/context/AssessmentContext";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -10,7 +11,9 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <div className="flex flex-col min-h-screen bg-[#0a0f1a]">
       <Toaster />
-      <main className="flex-grow">{children}</main>
+      <AssessmentProvider>
+        <main className="flex-grow">{children}</main>
+      </AssessmentProvider>
     </div>
   );
 }
